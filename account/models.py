21f2fc19441b939537c556ca392d4299a274a33b
katsapvlad <katsapvlad@yandex.by>
2021-07-20 18:12:38 +0300

    Refactoring forms

--- account/models.py
+++ account/models.py
@@ -4,9 +4,24 @@ from django.contrib.auth.models import User
 
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'type': 'password',
+                                                             'class': 'form-group first col-sm-3 form-control',
+                                                             'placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'type': 'password',
+                                                             'class': 'form-group first col-sm-3 form-control',
+                                                             'placeholder': 'Повторите пароль'}))
 
+    username = forms.CharField(label="", widget=forms.TextInput(attrs={'type': 'text',
+                                                             'class': 'form-group first col-sm-3 form-control',
+                                                             'placeholder': 'Введите никнейм'}))
+
+    first_name = forms.CharField(label="", widget=forms.TextInput(attrs={'type': 'text',
+                                                             'class': 'form-group first col-sm-3 form-control',
+                                                             'placeholder': 'Введите имя'}))
+
+    email = forms.CharField(label="", widget=forms.TextInput(attrs={'type': 'text',
+                                                             'class': 'form-group first col-sm-3 form-control',
+                                                             'placeholder': 'Введите почтовый адрес'}))
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email')
@@ -21,7 +36,7 @@ class UserRegistrationForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField(label="", widget=forms.TextInput(attrs={'type': 'text',
                                                              'class': 'form-group first col-sm-3 form-control',
-                                                             'placeholder': 'Ваше имя пользователя'}))
+                                                             'placeholder': 'Введите никнейм'}))
     password = forms.CharField(label="", widget=forms.PasswordInput(attrs={'type': 'password',
                                                              'class': 'form-group last col-sm-3 form-control',
-                                                             'placeholder': 'Ваш пароль'}))
+                                                             'placeholder': 'Введите пароль'}))
--- djangoProjectVNO/settings.py
+++ djangoProjectVNO/settings.py
@@ -141,12 +141,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     "static"
-# ]
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    "static"
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
