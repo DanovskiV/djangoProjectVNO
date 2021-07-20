6d80a6331731c7912091578b013414c3ff7b13ec
CenturionZ <wlad2104@yandex.by>
2021-07-20 09:45:33 +0300

    CSS fix

--- djangoProjectVNO/settings.py
+++ djangoProjectVNO/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
-# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     "static"
 # ]
