0ec77ccb4145e2babcbf0c2a7892d89a74d166b4
CenturionZ <wlad2104@yandex.by>
2021-08-03 09:27:39 +0300

    cors fix commented

--- djangoProjectVNO/settings.py
+++ djangoProjectVNO/settings.py
@@ -61,8 +61,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+# CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'djangoProjectVNO.urls'
 
