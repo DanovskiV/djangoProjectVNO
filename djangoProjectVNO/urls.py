39b57598a50edde6ad08946bca29e596977fc635
viva-la-radio <viva-la-radio@yandex.ru>
2022-09-15 14:28:35 +0300

    Revert "Update urls.py"
    This reverts commit 41109b23e505699c9b5ef73b7fdea710bf702e75.

--- djangoProjectVNO/urls.py
+++ djangoProjectVNO/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     url(r'^account/', include('account.urls')),
     # url(r'^account/', include('django.contrib.auth.urls')),
-    url(r'^media/', include('ckeditor_uploader.urls')),
+    url(r'^media/', include('media.urls')),
     url(r'^ckeditor', include('ckeditor_uploader.urls')),
     url(r'^questions/', include(('questions.urls', 'questions'), namespace="questions")),
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
