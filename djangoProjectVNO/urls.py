0b1ceeb83c6dba80972e8314e142e7ea6aceedc0
viva-la-radio <viva-la-radio@yandex.ru>
2022-09-15 15:32:24 +0300

    Картинки часть 4
    удалил записи из config.js

--- djangoProjectVNO/urls.py
+++ djangoProjectVNO/urls.py
@@ -25,8 +25,9 @@ sitemaps = {
 }
 
 urlpatterns = [
-    url(r'^ckeditor', include('ckeditor_uploader.urls')),
+    
     url('admin/', admin.site.urls),
+    url(r'^ckeditor', include('ckeditor_uploader.urls')),
     url(r'^', include(('blog.urls', 'blog'), namespace='blog')),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     url(r'^account/', include('account.urls')),
